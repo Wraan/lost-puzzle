f934efc0e1c3a4e69c000e4e23753fdd6032ef34
Kamil Drewniak <zibix12@gmail.com>
2018-10-16 21:27:57 +0200

    Adding roles for client 2

--- src/main/java/com/wran/controller/ClientController.java
+++ src/main/java/com/wran/controller/ClientController.java
@@ -1,12 +1,15 @@
 package com.wran.controller;
 
 import com.wran.model.Client;
+import com.wran.model.Role;
 import com.wran.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping(("/client"))
@@ -21,7 +24,7 @@ public class ClientController {
     }
     @GetMapping("/reg/{username}")
     public Client test(@PathVariable String username){
-        return clientService.signup(new Client(username, username, username+"@email.com"));
+        return clientService.signup(new Client(username, username, username+"@email.com", Arrays.asList(Role.ROLE_CLIENT)));
     }
 
 }
